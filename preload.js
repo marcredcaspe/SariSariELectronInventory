ae7f28221e51a69e2fc27d34de43b5f3fe36695f
marcredcaspe <marcredcaspe@gmail.com>
2025-10-15 11:19:35 +0800

    Delete preload.js

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  readInventory: () => ipcRenderer.invoke('read-inventory'),
-  writeInventory: (items) => ipcRenderer.invoke('write-inventory', items),
-  focusWindow: () => ipcRenderer.invoke('focus-window') // 👈 added
-});
